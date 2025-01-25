6a43b7d7a1152378b340b2477121dffa14074ea5
Betolicon <[albertlicontti@gmail.com]>
2025-01-25 09:32:25 -0400

    Changed the array and added the call to the print method

--- main.mjs
+++ main.mjs
@@ -1,8 +1,9 @@
 import pkg from './binaryTree.js'
 const { Tree } = pkg
 
-const array = []
+const array = [1, 3, 2, 1, 6, 5, 7]    
 
 const tree = new Tree(array)
+tree.prettyPrint()
 
-console.log(tree)
+///node main.mjs
